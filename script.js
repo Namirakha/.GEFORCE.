9dea53bf321d741eff4eb397d47cf6fb228496d1
Namirakha <namirakhan008@gmail.com>
2025-04-23 18:48:01 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,44 @@
+const defaultGroups = [
+  {
+    name: "Crypto Tools",
+    urls: [
+      "https://etherscan.io",
+      "https://coinmarketcap.com",
+      "https://uniswap.org"
+    ]
+  },
+  {
+    name: "News",
+    urls: [
+      "https://decrypt.co",
+      "https://cointelegraph.com"
+    ]
+  }
+];
+
+const groupList = document.getElementById("groupList");
+const openBtn = document.getElementById("openUrls");
+
+function renderGroups(groups) {
+  groupList.innerHTML = "";
+  groups.forEach((group) => {
+    const li = document.createElement("li");
+    li.innerHTML = `
+      <strong>${group.name}</strong>
+      <ul>
+        ${group.urls.map(url => `<li><a href="${url}" target="_blank">${url}</a></li>`).join("")}
+      </ul>
+    `;
+    groupList.appendChild(li);
+  });
+}
+
+openBtn.addEventListener("click", () => {
+  defaultGroups.flatMap(g => g.urls).forEach(url => window.open(url, "_blank"));
+});
+
+document.getElementById("toggleTheme").addEventListener("click", () => {
+  document.body.classList.toggle("dark-theme");
+});
+
+renderGroups(defaultGroups);
